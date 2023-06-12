5a4661d4beb3261938b744392dccb26423b6efd9
gilgamesh7 <felidaesrule@yahoo.com>
2023-06-12 19:03:03 +1200

    Use CPSR register for -ve numbers

--- addsubmul.s
+++ addsubmul.s
@@ -6,8 +6,9 @@ _start:
     MOV X1, #7
 
     ADD X2, X0, X1
-    SUB X2, X0, X1
-    MUL X2, X0, X1
+    SUB X3, X1, X0
+    SUBS X4, X0, X1 // Use CPSR register for negative number indication
+    MUL X5, X0, X1
 
     MOV X16, #1  // 1	AUE_EXIT	ALL	{ void exit(int rval); } 
 
